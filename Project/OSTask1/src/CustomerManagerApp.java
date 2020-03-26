cfe28cd23132d2ec64928a6145744f109cba63bd
Mahmoud Ahmed AbdElsalam <47575298+m9a9@users.noreply.github.com>
2020-03-26 09:29:53 +0200

    mahmoud ahmed

--- Project/OSTask1/src/CustomerManagerApp.java
+++ Project/OSTask1/src/CustomerManagerApp.java
@@ -22,15 +22,15 @@ public class CustomerManagerApp {
             action = Console.getString("Enter a command: ");
             System.out.println();
 
-            if (action.equalsIgnoreCase("list")) {
+            if (action.equalsIgnoreCase("1")) {
                 displayAllCustomers();
-            } else if (action.equalsIgnoreCase("add")) {
+            } else if (action.equalsIgnoreCase("2")) {
                 addCustomer();
-            } else if (action.equalsIgnoreCase("del") || action.equalsIgnoreCase("delete")) {
+            } else if (action.equalsIgnoreCase("3") || action.equalsIgnoreCase("delete")) {
                 deleteCustomer();
-            } else if (action.equalsIgnoreCase("help") || action.equalsIgnoreCase("menu")) {
+            } else if (action.equalsIgnoreCase("4") || action.equalsIgnoreCase("menu")) {
                 displayMenu();
-            } else if (action.equalsIgnoreCase("exit")) {
+            } else if (action.equalsIgnoreCase("5")) {
                 System.out.println("Bye.\n");
             } else {
                 System.out.println("Error! Not a valid command.\n");
@@ -40,11 +40,11 @@ public class CustomerManagerApp {
 
     public static void displayMenu() {
         System.out.println("COMMAND MENU");
-        System.out.println("list    - List all customers");
-        System.out.println("add     - Add a customer");
-        System.out.println("del     - Delete a customer");
-        System.out.println("help    - Show this menu");
-        System.out.println("exit    - Exit this application\n");
+        System.out.println("1    - List all customers");
+        System.out.println("2     - Add a customer");
+        System.out.println("3     - Delete a customer");
+        System.out.println("4    - Show this menu");
+        System.out.println("5    - Exit this application\n");
     }
 
     public static void displayAllCustomers() {
